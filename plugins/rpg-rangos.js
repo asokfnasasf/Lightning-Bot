f969ecae015ce2e0ed103f4d6cc3a050ea3d356c
asokfnasasf <121831160+asokfnasasf@users.noreply.github.com>
2023-01-04 19:00:17 -0300

    Update rpg-rangos.js

--- plugins/rpg-rangos.js
+++ plugins/rpg-rangos.js
@@ -172,7 +172,7 @@ ${role}
 *NOVATO(A) IV* 💩
 *NOVATO(A) V* 💩
 ╰━━━━━━━━━━━━━━━━━━━⬣`.trim()
-conn.sendHydrated(m.chat, menu, `𝙍𝘼𝙉𝙂𝙊𝙎 | ${wm}`, pp, 'https://github.com/GataNina-Li/GataBot-MD', '𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿', null, null, [
+conn.sendHydrated(m.chat, menu, `𝙍𝘼𝙉𝙂𝙊𝙎 | ${wm}`, pp, 'https://github.com/asokfnasasf/Lightning-Bot', '𝗟𝗶𝗴𝗵𝘁𝗻𝗶𝗻𝗴-𝗕𝗼𝘁', null, null, [
 ['𝙈𝙚𝙣𝙪́ 𝙘𝙤𝙢𝙥𝙡𝙚𝙩𝙤 | 𝙁𝙪𝙡𝙡 𝙈𝙚𝙣𝙪 💫', '.allmenu'],
 ['𝙏𝙤𝙥𝙨 | 𝙍𝙖𝙣𝙠𝙞𝙣𝙜 🏆', `${usedPrefix}top`],
 ['𝙈𝙚𝙣𝙪 𝙋𝙧𝙞𝙣𝙘𝙞𝙥𝙖𝙡 | 𝙈𝙖𝙞𝙣 𝙢𝙚𝙣𝙪 ⚡', '#menu']
