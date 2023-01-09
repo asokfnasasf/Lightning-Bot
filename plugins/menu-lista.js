3b2a71fb12c8353de7633fba938e9aa71340061d
asokfnasasf <121831160+asokfnasasf@users.noreply.github.com>
2023-01-09 18:09:14 -0300

    Update menu-lista.js

--- plugins/menu-lista.js
+++ plugins/menu-lista.js
@@ -29,7 +29,6 @@ const sections = [{
 title: comienzo + ' ' + lenguajeGB['smsLista1']() + ' ' + fin,
 rows: [
 {title: lenguajeGB['smsLista2'](), rowId: `${usedPrefix}creadora`},
-{title: lenguajeGB['smsLista3'](), rowId: `${usedPrefix}donar`},
 {title: lenguajeGB['smsLista4'](), rowId: `${usedPrefix}ping`},
 {title: lenguajeGB['smsLista5'](), rowId: `${usedPrefix}infomenu`},
 {title: lenguajeGB['smsLista6'](), rowId: `${usedPrefix}allmenu`},
@@ -62,8 +61,7 @@ rows: [
 {title: lenguajeGB['smsLista26'](), rowId: `${usedPrefix}stickermenu`},
 {title: lenguajeGB['smsLista27'](), rowId: `${usedPrefix}makermenu`},
 {title: lenguajeGB['smsLista28'](), rowId: `${usedPrefix}menulogos2`},
-{title: lenguajeGB['smsLista29'](), rowId: `${usedPrefix}randommenu`},
-{title: lenguajeGB['smsLista30'](), rowId: `${usedPrefix}hornymenu`, description: '\n'}
+{title: lenguajeGB['smsLista29'](), rowId: `${usedPrefix}randommenu`, description: '\n'}
 ]},{	
 title: comienzo + ' ' + lenguajeGB['smsLista31']() + ' ' + fin,
 rows: [
