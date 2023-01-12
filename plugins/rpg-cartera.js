0219891d68b4b61618b439034f5939cf8a0b64f1
Gabriel <121831160+asokfnasasf@users.noreply.github.com>
2023-01-12 15:44:03 -0300

    Update rpg-cartera.js

--- plugins/rpg-cartera.js
+++ plugins/rpg-cartera.js
@@ -4,9 +4,9 @@ let who
 if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
 else who = m.sender
 let name = conn.getName(who) 
-let grupos = [nna, nn, nnn, nnnt]
+let grupos = [nna]
 let gata = [img5, img6, img7, img8, img9]
-let enlace = { contextInfo: { externalAdReply: {title: wm + ' ⚡', body: 'support group' , sourceUrl: global.nna(), thumbnail: await(await fetch(gata.getRandom())).buffer() }}}
+let enlace = { contextInfo: { externalAdReply: {title: wm + ' ⚡', body: 'support group' , sourceUrl: grupos.getrandom(), thumbnail: await(await fetch(gata.getRandom())).buffer() }}}
 let enlace2 = { contextInfo: { externalAdReply: { showAdAttribution: true, mediaUrl: ig, mediaType: 'VIDEO', description: '', title: wm, body: ' 𝐒𝐮𝐩𝐞𝐫 𝐋𝐢𝐠𝐡𝐭𝐧𝐢𝐧𝐠𝐁𝐨𝐭 - 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 ', thumbnailUrl: await(await fetch(global.img)).buffer(), sourceUrl: ig }}}
 let dos = [enlace, enlace
 
@@ -20,7 +20,7 @@ const cartera = {
   },
 }
 const recursos = Object.keys(cartera.economia).map(v => user[v] && `*${global.rpgshop.emoticon(v)} ⇢ ${user[v]}*`).filter(v => v).join('\n').trim()
-await conn.sendButton(m.chat, `🎟️ 𝗣 𝗥 𝗘 𝗠 𝗜 𝗨 𝗠 ⇢ ${premium ? '✅' : '❌'}\n${wm}`, `👝 ⇢ ${name}\n` + recursos + `\n\n*PARA VER MÁS RECURSOS VISITE EL INVENTARIO*\n*TO SEE MORE RESOURCES VISIT THE INVENTORY*`, img5, [
+await conn.sendButton(m.chat, `🎟️ 𝗣 𝗥 𝗘 𝗠 𝗜 𝗨 𝗠 ⇢ ${premium ? '✔' : '✘'}\n${wm}`, `👝 ⇢ ${name}\n` + recursos + `\n\n*PARA VER MÁS RECURSOS VISITE EL INVENTARIO*\n*TO SEE MORE RESOURCES VISIT THE INVENTORY*`, img5, [
 ['𝙄𝙣𝙫𝙚𝙣𝙩𝙖𝙧𝙞𝙤 | 𝙄𝙣𝙫𝙚𝙣𝙩𝙤𝙧𝙮 🎒', '/inventario'],
 ['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪 ☘️', '/menu']], m, enlace)  
 }
