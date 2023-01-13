6ca81ebef637fc9fd21215c9f26254bf741ffe47
Gabriel <121831160+asokfnasasf@users.noreply.github.com>
2023-01-13 12:16:09 -0300

    Update config.js

--- config.js
+++ config.js
@@ -346,7 +346,7 @@ global.rpg = {
       kambing: '🐐 Cabra : Goat',
       kerbau: '🐃 Búfalo : Buffalo',
       lion: '🦁 León : Lion',
-      money: lenguajeGB.eGataCoins(),
+      money: lenguajeGB.eLightCoins(),
       monyet: '🐒 Mono : Monkey',
       panda: '🐼 Panda',
       snake: '🐍 Serpiente : Snake',
